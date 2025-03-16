cb8605e1953fb07948d2d850752c3d1a92366b24
Akhil Kasturi <tuk21380@temple.edu>
2025-03-16 14:08:02 -0400

    Updated side view images for burger component. Mustard, ketchup, and cheese still need to be adjusted, but the rest of the ingredients should stack better now.

--- frontend/src/components/BurgerBuilder.js
+++ frontend/src/components/BurgerBuilder.js
@@ -4,19 +4,19 @@ const BurgerBuilder = () => {
     const [ingredients, setIngredients] = useState([]);
 
     const foodItems = [
-        { id: 2, name: 'Bottom Bun', image: '/images/bottom_bun.png', audio: '/audio/bottom_bun.mp3' },
-        { id: 3, name: 'Top Bun', image: '/images/top_bun.png', audio: '/audio/top_bun.mp3' },
-        { id: 4, name: 'Patty', image: '/images/patty.png', audio: '/audio/patty.mp3' },
-        { id: 6, name: 'Lettuce', image: '/images/lettuce.png', audio: '/audio/lettuce.mp3' },
-        { id: 7, name: 'Onion', image: '/images/onion.png', audio: '/audio/onion.mp3' },
-        { id: 8, name: 'Tomato', image: '/images/tomato.png', audio: '/audio/tomato.mp3' },
-        { id: 9, name: 'Ketchup', image: '/images/ketchup.png', audio: '/audio/ketchup.mp3' },
-        { id: 10, name: 'Mustard', image: '/images/mustard.png', audio: '/audio/mustard.mp3' },
-        { id: 11, name: 'Cheese', image: '/images/cheese.png', audio: '/audio/cheese.mp3' },
+        { id: 2, name: 'Bottom Bun', image: '/images/bottom_bun.png', audio: '/audio/bottom_bun.mp3', sideImage: '/images/BottomBunSide.png' },
+        { id: 3, name: 'Top Bun', image: '/images/top_bun.png', audio: '/audio/top_bun.mp3', sideImage: '/images/TopBunSide.png' },
+        { id: 4, name: 'Patty', image: '/images/patty.png', audio: '/audio/patty.mp3', sideImage: '/images/PattySide.png' },
+        { id: 6, name: 'Lettuce', image: '/images/lettuce.png', audio: '/audio/lettuce.mp3', sideImage: '/images/LettuceSide.png' },
+        { id: 7, name: 'Onion', image: '/images/onion.png', audio: '/audio/onion.mp3', sideImage: '/images/OnionSide.png' },
+        { id: 8, name: 'Tomato', image: '/images/tomato.png', audio: '/audio/tomato.mp3', sideImage: '/images/TomatoSide.png' },
+        { id: 9, name: 'Ketchup', image: '/images/ketchup.png', audio: '/audio/ketchup.mp3', sideImage: '/images/KetchupSide.png' },
+        { id: 10, name: 'Mustard', image: '/images/mustard.png', audio: '/audio/mustard.mp3', sideImage: '/images/MustardSide.png' },
+        { id: 11, name: 'Cheese', image: '/images/cheese.png', audio: '/audio/cheese.mp3', sideImage: '/images/CheeseSide.png' },
     ];
 
     const addIngredient = (ingredient) =>{
-        setIngredients([...ingredients,ingredient]);
+        setIngredients([...ingredients,ingredient.sideImage]);
     };
     return(
         <div className="BurgerBuilder">
@@ -31,16 +31,16 @@ const BurgerBuilder = () => {
             </div>
 
             <div className = "Burger">
-                {ingredients.map((ingredient,index) => (
+                {ingredients.map((image,index) => (
                     <img
                         key={index}
-                        src={ingredient.image}
-                        alt={ingredient.name}
+                        src={image}
+                        alt={'Ingredient ${index}'}
                         className="BurgerIngredient"
                     />
                 ))}
             </div>
-            <div class="Plate">
+            <div className="Plate">
             </div>
             <h2>Your Plate</h2>
         </div>
