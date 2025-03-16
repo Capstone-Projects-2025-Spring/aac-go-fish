6e1c2b5f31e14c668be5be8de1f04e128ac95c14
Jonathan Zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2025-03-16 12:13:28 -0400

    Remove channel in favor of asyncio Queue

--- backend/game_state.py
+++ backend/game_state.py
@@ -1,28 +1,29 @@
 from __future__ import annotations
 
+import asyncio
 import dataclasses
-import itertools
-from collections.abc import Iterable
+from collections.abc import AsyncIterable
 from dataclasses import dataclass
 from uuid import uuid4
 
-from .channel import Channel
 from .models import Message, Role
 
 
 @dataclass
 class Lobby:
     """
-    A class representing a game state.
+    Maintains lobby state and allows communication to and from players.
 
     Attributes:
-        id (str): The lobby's internal ID
-        players (dict[str, Player]): Map of player id to Players.
-        code (str): The code used to join the lobby
-        started (bool): Whether the game has started
+        id: The lobby's internal ID
+        players: Map of player id to Players.
+        channel: Message queue for incoming messages.
+        code: The code used to join the lobby
+        started: Whether the game has started
     """
 
     players: dict[str, Player]
+    channel: asyncio.Queue
     id: str = dataclasses.field(init=False, default_factory=lambda: uuid4().hex)
     code: str = "ABC"
     started: bool = False
@@ -32,24 +33,24 @@ class Lobby:
         for player in self.players.values():
             await player.send(msg)
 
-    async def messages(self) -> Iterable[Message]:
+    async def messages(self) -> AsyncIterable[Message]:
         """Consume messages from all players."""
-
-        return []
+        while True:
+            yield await self.channel.get()
 
 
 @dataclass
 class Player:
     """
-    A class representing a player's ingame data.
+    A player in a lobby.
 
     Attributes:
-        id (str): The player's internal ID
-        role (Role): The player's role.
-        channel (Channel): Channel to send messages to frontend.
+        id: The player's internal ID
+        role: The player's role.
+        channel: Message queue for outgoing messages.
     """
 
-    channel: Channel
+    channel: asyncio.Queue
     role: Role
     id: str = dataclasses.field(init=False, default_factory=lambda: uuid4().hex)
 
--- backend/tests/test_game_state.py
+++ backend/tests/test_game_state.py
@@ -1,53 +1,45 @@
+import asyncio
+
 import pytest
 
-from backend.channel import channel
 from backend.game_state import Lobby, Player
 from backend.models import Role
 
+type LobbyFixture = tuple[Lobby, asyncio.Queue[int], asyncio.Queue[int], asyncio.Queue[int]]
+
+
+@pytest.fixture
+def lobby() -> LobbyFixture:
+    """Setup a Lobby with channels for 2 players."""
+    lobby_q = asyncio.Queue()
+    a_q = asyncio.Queue()
+    b_q = asyncio.Queue()
+
+    lobby = Lobby({"A": Player(a_q, Role.manager), "B": Player(b_q, Role.burger)}, lobby_q)
+
+    return lobby, lobby_q, a_q, b_q
+
 
 @pytest.mark.asyncio
-async def test_lobby_broadcast() -> None:
+async def test_lobby_broadcast(lobby: LobbyFixture) -> None:
     """Broadcasted messages are received from multiple players."""
-    left_a, right_a = channel()
-    left_b, right_b = channel()
+    lobb, _, a_q, b_q = lobby
 
-    lobby = Lobby({"A": Player(left_a, Role.manager), "B": Player(left_b, Role.burger)})
+    await lobb.broadcast(3)  # pyright: ignore[reportArgumentType]
 
-    msg = 5
-    await lobby.broadcast(msg)  # pyright: ignore[reportArgumentType]
-
-    assert right_a.get() == msg
-    assert right_b.get() == msg
+    assert await a_q.get() == 3
+    assert await b_q.get() == 3
 
 
 @pytest.mark.asyncio
-async def test_messages_receives_msg() -> None:
+async def test_messages_receives_msg(lobby: LobbyFixture) -> None:
     """Test iterator over multiple players."""
-    left_a, right_a = channel()
-    left_b, right_b = channel()
+    lobb, lobby_q, _, _ = lobby
 
-    lobby = Lobby({"A": Player(left_a, Role.manager), "B": Player(left_b, Role.burger)})
+    await lobby_q.put(3)
+    await lobby_q.put(6)
 
-    await right_a.put(4)
-    await right_b.put(8)
+    i = aiter(lobb.messages())
 
-    messages = iter(await lobby.messages())
-    assert next(messages) == 4
-    assert next(messages) == 8
-
-
-@pytest.mark.asyncio
-async def test_messages_skips_none() -> None:
-    """Test that the iterator skips over empty player message channels."""
-    left_a, right_a = channel()
-    left_b, _ = channel()
-
-    lobby = Lobby({"A": Player(left_a, Role.manager), "B": Player(left_b, Role.burger)})
-
-    # two messages from A: we should get both without any Nones in between
-    await right_a.put(4)
-    await right_a.put(8)
-
-    messages = iter(await lobby.messages())
-    assert next(messages) == 4
-    assert next(messages) == 8
+    assert await anext(i) == 3
+    assert await anext(i) == 6
