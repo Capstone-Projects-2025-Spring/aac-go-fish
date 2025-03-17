2c1776f740c9f7f5ba346a39f0402665a81d1a19
Jonathan Zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2025-03-17 15:36:52 -0400

    Document role assignment rules

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -13,6 +13,10 @@ class _LobbyManager:
         """
         Add a player to a lobby given a lobby join code.
 
+        Role is assigned based on order of joining. The first player has the
+        manager role, the second has the burger role, the third has the fry
+        role, the fourth has the drink role.
+
         Args:
             code: The lobby join code.
 
