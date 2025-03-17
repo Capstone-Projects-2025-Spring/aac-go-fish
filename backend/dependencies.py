9fe5bce47022a11963ab41e046f247714c7736f2
Jonathan Zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2025-03-17 15:21:02 -0400

    Add lobby manager dependency scaffolding

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,8 +1,42 @@
 import asyncio
+from typing import Callable
+from backend.game_state import Lobby
 
-Q = asyncio.Queue(maxsize=10000)
+class _LobbyManager:
+    """Handle creation of lobbies and adding players to lobbies."""
+
+    def __init__(self, code_generator: Callable[[], str]) -> None:
+        self.lobbies: dict[str, Lobby] = {}
+        self.code_generator = code_generator
+
+    def register_player(self, code: str) -> str:
+        """
+        Add a player to a lobby given a lobby join code.
+
+        Args:
+            code: The lobby join code.
+
+        Raises:
+            ValueError: The code is invalid.
+
+        Returns:
+            The id of the newly created player.
+        """
+
+        return ""
+
+    def register_lobby(self) -> str:
+        """
+        Create a new lobby.
+
+        Returns:
+            The lobby's join code.
+        """
+        code = self.code_generator()
+        l = Lobby({}, asyncio.Queue(), code)
+
+        self.lobbies[code] = l
+        return code
 
 
-def event_queue() -> asyncio.Queue:
-    """Returns the event queue."""
-    return Q
+LobbyManager = _LobbyManager(lambda: "code")
