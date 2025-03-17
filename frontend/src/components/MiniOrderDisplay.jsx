a4e08f46df29f36ff6d0b4016eedc84f319611c1
tuo20482 <tuo20482@temple.edu>
2025-03-17 19:48:48 -0400

    Mini display component

---
+++ frontend/src/components/MiniOrderDisplay.jsx
@@ -0,0 +1,25 @@
+import BurgerDisplay from "./BurgerDisplay";
+import SideDisplay from "./SideDisplay";
+import DrinkDisplay from "./DrinkDisplay";
+import "./MiniOrderDisplay.css"
+import React from "react";
+
+export default function MiniOrderDisplay({ burger, side, drink }) {
+    return (<>
+        <h2>Order (from employees)</h2>
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "left",
+            transform: "scale(0.5)",
+            height: "15.0rem",
+            width: "30.0rem",
+            gap: "15.0rem"
+        }}>
+            {burger !== null && (<BurgerDisplay imagePaths={burger.map((ingredient) => ingredient.sideImage)}/>)}
+            {side !== null && (<SideDisplay tableState={side.tableState}/>)}
+            {drink !== null && (<DrinkDisplay layers={drink.layers} hasIce={drink.hasIce}/>)}
+        </div>
+        </>
+    );
+}
