3adbb673396b322ec7602ed93e53a82f036c2720
Matt-Littlefield <matt.littlefield@temple.edu>
2025-03-23 16:36:26 -0400

    update formatting

--- frontend/src/MockOrders.js
+++ frontend/src/MockOrders.js
@@ -7,11 +7,5 @@ export const mockBurgerOrders = [
 export const mockDrinkOrders = [
     ['Yellow', 'Red', 'Green', 'Blue', 'Orange', 'Red', 'Green', 'Yellow', 'Orange', 'Green'],
     ['Blue', 'Red', 'Blue', 'Green', 'Orange', 'Purple', 'Red', 'Green','Purple', 'Orange'],
-    ['Purple', 'Red', 'Blue','Purple', 'Green', 'Yellow', 'Orange', 'Red', 'Orange', 'Purple', 'Red'],
-];
-
-export const mockSideOrders = [
-    ['Bottom Bun', 'Mustard', 'Lettuce', 'Tomato', 'Patty', 'Cheese', 'Ketchup', 'Top Bun'],
-    ['Bottom Bun', 'Ketchup', 'Lettuce', 'Onion', 'Patty', 'Cheese', 'Onion', 'Ketchup','Top Bun'],
-    ['Bottom Bun', 'Tomato', 'Patty','Onion', 'Mustard', 'Ketchup', 'Top Bun'],
+    ['Purple', 'Red', 'Blue','Purple', 'Green', 'Yellow', 'Orange', 'Red', 'Orange', 'Purple'],
 ];
