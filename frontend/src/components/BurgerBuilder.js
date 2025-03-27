3e6c1efcbff9837c2cb2a271b9eb3e30381bd7cf
tuo20482 <tuo20482@temple.edu>
2025-03-27 17:46:19 -0400

    Refactor burger side images to reduce code duplication

--- frontend/src/components/BurgerBuilder.js
+++ frontend/src/components/BurgerBuilder.js
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
 import './BurgerBuilder.css';
 import BurgerDisplay from "./BurgerDisplay";
+import {menu} from "../menuItems";
 const BurgerBuilder = ({ onSend }) => {
     const [ingredients, setIngredients] = useState([]);
 
-    const foodItems = [
-        { id: 2, name: 'Bottom Bun', image: '/images/bottom_bun.png', audio: '/audio/bottom_bun.mp3', sideImage: '/images/BottomBunSide.png' },
-        { id: 3, name: 'Top Bun', image: '/images/top_bun.png', audio: '/audio/top_bun.mp3', sideImage: '/images/TopBunSide.png' },
-        { id: 4, name: 'Patty', image: '/images/patty.png', audio: '/audio/patty.mp3', sideImage: '/images/PattySide.png' },
-        { id: 6, name: 'Lettuce', image: '/images/lettuce.png', audio: '/audio/lettuce.mp3', sideImage: '/images/LettuceSide.png' },
-        { id: 7, name: 'Onion', image: '/images/onion.png', audio: '/audio/onion.mp3', sideImage: '/images/OnionSide.png' },
-        { id: 8, name: 'Tomato', image: '/images/tomato.png', audio: '/audio/tomato.mp3', sideImage: '/images/TomatoSide.png' },
-        { id: 9, name: 'Ketchup', image: '/images/ketchup.png', audio: '/audio/ketchup.mp3', sideImage: '/images/KetchupSide.png' },
-        { id: 10, name: 'Mustard', image: '/images/mustard.png', audio: '/audio/mustard.mp3', sideImage: '/images/MustardSide.png' },
-        { id: 11, name: 'Cheese', image: '/images/cheese.png', audio: '/audio/cheese.mp3', sideImage: '/images/CheeseSide.png' },
-    ];
+    const foodItems = menu[0].children;
 
     const handleSend = () => {
         onSend(ingredients);
--- frontend/src/components/CustomerOrder.js
+++ frontend/src/components/CustomerOrder.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './CustomerOrder.css';
+import {drinkColors} from "./DrinkBuilder";
+import {menu} from "../menuItems";
 
 function CustomerOrder({
     burgerOrder,
@@ -10,94 +12,43 @@ function CustomerOrder({
     orderVisible
 }) {
 
-    const foodItems = [
-        { id: 2, name: 'Bottom Bun', image: '/images/bottom_bun.png', audio: '/audio/bottom_bun.mp3', sideImage: '/images/BottomBunSide.png' },
-        { id: 3, name: 'Top Bun', image: '/images/top_bun.png', audio: '/audio/top_bun.mp3', sideImage: '/images/TopBunSide.png' },
-        { id: 4, name: 'Patty', image: '/images/patty.png', audio: '/audio/patty.mp3', sideImage: '/images/PattySide.png' },
-        { id: 6, name: 'Lettuce', image: '/images/lettuce.png', audio: '/audio/lettuce.mp3', sideImage: '/images/LettuceSide.png' },
-        { id: 7, name: 'Onion', image: '/images/onion.png', audio: '/audio/onion.mp3', sideImage: '/images/OnionSide.png' },
-        { id: 8, name: 'Tomato', image: '/images/tomato.png', audio: '/audio/tomato.mp3', sideImage: '/images/TomatoSide.png' },
-        { id: 9, name: 'Ketchup', image: '/images/ketchup.png', audio: '/audio/ketchup.mp3', sideImage: '/images/KetchupSide.png' },
-        { id: 10, name: 'Mustard', image: '/images/mustard.png', audio: '/audio/mustard.mp3', sideImage: '/images/MustardSide.png' },
-        { id: 11, name: 'Cheese', image: '/images/cheese.png', audio: '/audio/cheese.mp3', sideImage: '/images/CheeseSide.png' },
-    ];
-
-    const drinkLayers = [
-        {name: "Blue", color: "#0033CC"},
-        {name: "Green", color: "#00CC00"},
-        {name: "Yellow", color: "#FFFF00"},
-        {name: "Red", color: "#FF0000"},
-        {name: "Orange", color: "#FF9900"},
-        {name: "Purple", color: "#660099"},
-    ];
+    const burgerIngredients = menu[0].children;
 
     // Temporarily set hasIce to false until ice is fully implemented in the drink station
     hasIce = false;
     return (
-        <div className = "CustomerOrder">
-            <div className = "orderDisplay">
-                <div className = "burgerOrderDisplay">
-                    {[...burgerOrder].map((itemName,index) => {
-                        const item = foodItems.find(food => food.name === itemName);
-                        return item ? (
-                            <div key={index} className="foodItem">
-                                <img src={item.sideImage} alt={item.sideImage}/>
-                            </div>
-                        ) : null;
-                    })}
-                </div>
-                {orderVisible && hasSide ? (
-                    <div className='sideDisplay'>
-                        {
-                        <img
-                            src="/images/fries.png"
-                            alt="Fries"
-                            className="Fries"
-                        />
-                        }
-                    </div>
-                ): orderVisible ? (
-                    <div className='sideDisplay'>
-                        {
-                        <img
-                            src="/images/noFries.png"
-                            alt="NoFries"
-                            className="NoFries"
-                        />
-                        }
-                    </div>
-                )
-                : null
-                }
-                {orderVisible ? (
-                    <div className="mockDisplayCup" style = {{ backgroundColor: drinkOrder[1], color: "#FFFFFF" }}>
-
-                        <p className='drinkSizeText'>
-                        {drinkSize === 'large' ?
-                            "L"
-                        : drinkSize === 'medium' ?
-                            "M"
-                        : drinkSize === 'small' ?
-                            "S"
-                         : null
-                        }
-                        </p>
-
-                        {hasIce && (
+        <div className="CustomerOrder">
+            <div className="orderDisplay">
+                {orderVisible && (
+                    <>
+                        <div className="burgerOrderDisplay">
+                            {[...burgerOrder].map((ingredientName, index) => {
+                                const ingredient = burgerIngredients.find(food => food.name === ingredientName);
+                                return ingredient && (
+                                    <div key={index} className="foodItem">
+                                        <img src={ingredient.sideImage} alt={ingredient.name}/>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div className="sideDisplay">
                             <img
-                                src="/images/ice.png"
-                                alt="Ice"
-                                className="Ice"
+                                src={`/images/${hasSide ? "fries" : "noFries"}.png`}
+                                alt={hasSide ? "Fries" : "NoFries"}
+                                className={hasSide ? "Fries" : "NoFries"}
                             />
-                        )}
-                    </div>
-                    )
-                : null
-                }
+                        </div>
+                        <div className="mockDisplayCup" style={{backgroundColor: drinkOrder[1], color: "#FFFFFF"}}>
+                            <p className="drinkSizeText">
+                                {drinkSize ? drinkSize[0].toUpperCase() : null}
+                            </p>
+                            {hasIce && <img src="/images/ice.png" alt="Ice" className="Ice"/>}
+                        </div>
+                    </>)}
             </div>
-
         </div>
     );
-};
+}
+
 
 export default CustomerOrder;
