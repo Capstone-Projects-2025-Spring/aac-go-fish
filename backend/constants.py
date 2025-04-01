ed4be72011d36d0f93844fd49166a8c775cf8cd2
tuo20482 <tuo20482@temple.edu>
2025-04-01 13:44:55 -0400

    Move frontend URL to Settings

--- backend/constants.py
+++ backend/constants.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     """App settings."""
 
     env: str = "dev"
-
+    frontend_url: str = "http://localhost:3000"
     log_level: str = "DEBUG"
 
 
