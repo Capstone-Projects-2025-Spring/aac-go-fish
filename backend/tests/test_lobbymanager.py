20f095427694f4be7655e4b69d4e3306e2ef1959
Andriy Luchko <tul33640@temple.edu>
2025-04-06 16:17:53 -0400

    Add more tests for code generation

--- backend/tests/test_lobbymanager.py
+++ backend/tests/test_lobbymanager.py
@@ -17,6 +17,24 @@ def test_register_lobby(lm: LobbyManager) -> None:
     assert all(ingredient in BURGER_INGREDIENTS for ingredient in code)
 
 
+def test_no_more_lobbies(lm: LobbyManager) -> None:
+    """Test RuntimeError is thrown when no more lobbies are available."""
+    for _ in range(len(lm.available_codes)):
+        lm.register_lobby()
+
+    with pytest.raises(RuntimeError) as exc_info:
+        lm.register_lobby()
+
+    assert exc_info.type is RuntimeError
+    assert str(exc_info.value) == "No more lobby codes available, server is full"
+
+
+def test_different_lobbies_have_different_codes(lm: LobbyManager) -> None:
+    """Test that different lobbies have different codes."""
+    codes = [lm.register_lobby() for _ in range(10)]
+    assert len(codes) == len(set(tuple(code) for code in codes))
+
+
 def test_register_player_invalid_code(lm: LobbyManager) -> None:
     """Test ValueError is thrown on invalid codes."""
     with pytest.raises(ValueError) as exc_info:
