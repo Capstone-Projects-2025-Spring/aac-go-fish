8bacd528fb83d03006ff4e4eaefc586ac764b4a2
tjmcb <tuo70963@temple.edu>
2025-04-08 16:51:18 -0400

    Link icon replaces copy link button

--- frontend/src/components/Lobby.jsx
+++ frontend/src/components/Lobby.jsx
@@ -1,6 +1,7 @@
 import React, {useContext, useState} from 'react';
 import './Lobby.css';
 import { useWebSocket, WebSocketContext } from '../WebSocketContext';
+import { FaLink } from "react-icons/fa";
 
 function Lobby({ lobbyCode }) {
     const [playerCount, setPlayerCount] = useState(1);
@@ -35,7 +36,7 @@ function Lobby({ lobbyCode }) {
     return (
         <div className="lobby-content">
             <div className="player-status">
-                <h3>Players in Lobby: {Math.min(playerCount, 4)} / 4 <button onClick={copyCode}>Copy Link</button></h3>
+                <h3>Players in Lobby: {Math.min(playerCount, 4)} / 4 <FaLink onClick={copyCode}/></h3>
                 {playerCount < 2 && <h4>Waiting for more players to join...</h4>}
                 {(playerCount == 2 || playerCount == 3) && <h4>You have company! Wait for more players or start game now!</h4>}
                 {playerCount >= 4 && <h4>Max players have joined!</h4>}
