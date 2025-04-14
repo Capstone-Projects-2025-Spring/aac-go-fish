d2c424f86da59cb3c7c29ef5c1b3841bad1303c9
tjmcb <tuo70963@temple.edu>
2025-04-14 16:02:50 -0400

    score formatted in USD

--- frontend/src/components/Score/Score.jsx
+++ frontend/src/components/Score/Score.jsx
@@ -1,9 +1,17 @@
 import "./Score.css";
 
 const Score = ({ score, day }) =>{
+    // score is given in cents, divide by 100 and format
+    const formattedScore = formatter.format(score / 100);
+
     return <div className="Score">
-        <p className="ScoreText">Day {day} - ${score}</p>
+        <p className="ScoreText">Day {day} - {formattedScore}</p>
     </div>
 }
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
 
 export default Score;
