152260c0892058cd8a3438fdb2f0845b264815bf
tuo20482 <tuo20482@temple.edu>
2025-04-18 13:01:20 -0400

    Update wording

--- frontend/src/components/GameCompleteModal/GameCompleteModal.jsx
+++ frontend/src/components/GameCompleteModal/GameCompleteModal.jsx
@@ -4,7 +4,7 @@ import ReactDom from "react-dom"
 export default function GameCompleteModal({ score }) {
     return ReactDom.createPortal(<div className="modal-overlay">
         <div className="modal-content">
-            <div className="text">🎉You win!🎉</div>
+            <div className="text">🎉Great job!🎉</div>
             <div className="score-earned">${score}</div>
             <button onClick={() => window.location.href = "/"} className="home-button">
                 🏠 Back to home
